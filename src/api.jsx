cbaab127931092666aa87e16b9b98b53fbc41927
Juan David Peña <juandavidp76@hotmail.com>
2024-04-03 12:30:30 -0500

    delete comments

--- src/api.jsx
+++ src/api.jsx
@@ -16,31 +16,13 @@ export async function fetchGroups(town, radius) {
     const data = response.data;
     const filteredGroups = data.data.filter((group) => {
       return (
-        group.privacy === 'CLOSED' && // private groups
-        group.member_count > 1000 && // over 1000 members
-        group.name.toLowerCase().includes(town.toLowerCase()) // within the specified town
+        group.privacy === 'CLOSED' &&
+        group.member_count > 1000 &&
+        group.name.toLowerCase().includes(town.toLowerCase())
       );
     });
     return filteredGroups;
   } catch (error) {
     throw new Error('Failed to fetch groups');
   }
-
-  // try {
-  //   const response = await fetch(url);
-  //   if (!response.ok) {
-  //     throw new Error('Failed to fetch groups');
-  //   }
-  //   const data = await response.json();
-  //   const filteredGroups = data.data.filter(group => {
-  //     return (
-  //       group.privacy === 'CLOSED' && // private groups
-  //       group.member_count > 1000 && // over 1000 members
-  //       group.name.toLowerCase().includes(town.toLowerCase()) // within the specified town
-  //     );
-  //   });
-  //   return filteredGroups;
-  // } catch (error) {
-  //   throw new Error('Failed to fetch groups');
-  // }
 }
