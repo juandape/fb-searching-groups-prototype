77fb6928592069eeb354ebed3eb5633e022b2585
Juan David Peña <juandavidp76@hotmail.com>
2024-04-04 20:45:58 -0500

    feat: links

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -5,17 +5,23 @@ function SearchResult({ groups }) {
     <>
       <hr />
       <h2 className='text-center text-3xl font-bold mt-5'>Search Results</h2>
-      <ul className=''>
+      <ul className='mt-10'>
         {groups.map((group) => (
           <li key={group.id} className='border-2 w-80 mb-4 mx-auto'>
-            <h3 className='text-xl text-blue-500'>
-              Group Name: {group.groups}
-            </h3>
-            <p>State: {group.state}</p>
-            <p>Members: {group.Members}</p>
-            <p>Distance: {group.radius} miles</p>
-            <p>Town: {group.town}</p>
-            <p>Type: {group.type}</p>
+            <a
+              href={group.link}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <p className='text-xl text-blue-500'>
+                Group Name: {group.groups}
+              </p>
+              <p>State: {group.state}</p>
+              <p>Members: {group.Members}</p>
+              <p>Distance: {group.radius} miles</p>
+              <p>Town: {group.town}</p>
+              <p>Type: {group.type}</p>
+            </a>
           </li>
         ))}
       </ul>
